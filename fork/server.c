29c4c3c3fd5ef40068625a8b16c3252bbce62c7e
shurinm <shurin-m@mail.ru>
2016-07-20 14:50:56 +0600

    first commit

---
+++ fork/server.c
@@ -0,0 +1,74 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main ()
+{
+	int server_sock_tcp, sock;				//дискрипторы сокетов
+	struct sockaddr_in addr;				//структура с адресом
+	char buf[2048];       					// буфур для приема
+    int bytes_read;           				// кол-во принятых байт
+    time_t utime;                      		 // Текущее время в микросекундах
+    struct tm *tmp;                  	 	  // Абсолютное текущее время
+    char msg[256];							// Получаемое сообщение
+
+	server_sock_tcp = socket(AF_INET, SOCK_STREAM, 0);	//создаем сокет для входных подключений
+	if(server_sock_tcp < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+    //задаем параметры сервера
+    addr.sin_family = AF_INET;
+ 	addr.sin_port = htons(3428);
+    //addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    if(bind(server_sock_tcp, (struct sockaddr *)&addr, sizeof(addr)) < 0) // связываемся с сетевым устройство. 
+    //Сейчас это устройство lo - "петля", которое используется для отладки сетевых приложений
+    {
+        perror("bind");
+        exit(2);
+    }
+    listen(server_sock_tcp, 1); // очередь входных подключений
+    sock = accept(server_sock_tcp, NULL, NULL); // принимаем входные подключение и создаем отделный сокет для каждого нового подключившегося клиента
+    if(sock < 0)
+    {
+    	perror("Прием входящих подключений");
+    	exit(3);
+    }
+    	
+        switch (fork())
+        {
+        	case 0:
+        		while( 1 )
+    {
+    	bytes_read = recv(sock, buf, 2048, 0); // принимаем сообщение от клиента
+            if(bytes_read == 0)
+            	break;
+    	printf("Получено %d bytes\tСообщение: %s\n", bytes_read, buf);
+    	// Формируем время в нужном формате и отправляем
+        utime = time(NULL);
+        tmp = localtime(&utime);
+        strftime(msg, 256, "%H:%M:%S", tmp);
+        send(sock, msg, 256, 0); // отправляем текущее время
+        // send(int sockfd, const void *buf, size_t len, int flags);
+    }
+            case -1:
+                perror("fork");
+            break;
+            default:
+            close(sock); // закрываем сокет  
+  			break;
+        }
+    close(sock); // закрываем сокет  
+    return 0;
+}
+
